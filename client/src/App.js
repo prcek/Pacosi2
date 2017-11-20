f3928ede6360f6ab2ae216cb43b1323d52bf71a8
Tomas Hluchan <tomas.hluchan@gmail.com>
2017-11-20 14:26:36 +0100

    init

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {users: []}
+  
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+  
+
   render() {
     return (
       <div className="App">
@@ -13,6 +23,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
