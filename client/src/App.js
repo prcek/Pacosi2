ea2368ed3fbde944e5214952e58471954f722b05
Tomas Hluchan <tomas.hluchan@gmail.com>
2017-12-26 23:04:49 +0100

    no location info

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,15 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { connect } from 'react-redux'
 
+import { SnackbarContent } from 'material-ui/Snackbar';
+
+
 const CurrentUsers = gql`
   query CurrentUsers {
     users {
       id
       name
       email
-    }
-    locations {
-      id,name
     }
   }
 `;
@@ -45,14 +45,17 @@ class App extends Component {
         <header className="App-header">
           <MenuBar />
         </header>
+
+        {! this.props.current_location_id && <SnackbarContent message="neni zvolena lokalita"/>}
+
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Users</h1>
         {this.props.data.loading ? <div> loading </div>: this.renderUsers(this.props.data.users) }
-        
-        <Typography> Hi this is Typography </Typography>
-        <AccessAlarmIcon/>
+        <Typography> Hi </Typography>
+        <AccessAlarmIcon />
         {this.props.current_location_id ? <div> current location is {this.props.current_location_id} </div> : <div> no current location </div> }
       </div>
     );
