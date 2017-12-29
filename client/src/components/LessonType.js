f983e6b036a0777b6ab91c207c8921e4a339d52b
Tomas Hluchan <tomas.hluchan@gmail.com>
2017-12-29 16:20:34 +0100

    up

--- client/src/components/LessonType.js
+++ client/src/components/LessonType.js
@@ -5,12 +5,17 @@ import Typography from 'material-ui/Typography';
 import { compose } from 'react-apollo'
 import InfiniteCalendar from 'react-infinite-calendar';
 import 'react-infinite-calendar/styles.css';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import AddIcon from 'material-ui-icons/Add';
 
 const styles = theme => ({
     root: {
       marginTop: theme.spacing.unit * 3,
       width: '100%',
     },
+
 });
   
 
@@ -20,6 +25,8 @@ class LessonType extends React.Component {
     renderCal() {
         return (
             <InfiniteCalendar 
+                width={"100%"}
+                height={400}
                 autoFocus={false} 
                 displayOptions={{
                     showHeader: false
@@ -48,12 +55,34 @@ class LessonType extends React.Component {
         );
     }
 
+    renderLessons() {
+        return (
+            <Paper>
+                <Tabs value={1} scrollable scrollButtons="auto">
+                    <Tab label="Lekce 12:30, 2/10" />
+                    <Tab label="Lekce 11:00, 0/11" />
+                    <Tab label="Lekce 0:00, 11/10"/>
+                    <Tab icon={<AddIcon/>} />
+                </Tabs>
+            </Paper>
+        )
+    }
+
     render() {
         const cal = this.renderCal();
+        const lessons = this.renderLessons();
         return (
             <div>
             <Typography> I Am LessonType (id:{this.props.lessonTypeId}) </Typography>
-            {cal}
+
+            <Grid container>
+                <Grid item xs={12} sm={5} md={4} lg={3}>
+                    {cal}
+                </Grid>
+                <Grid item xs={12} sm={7} md={8} lg={9}>
+                    {lessons}
+                </Grid>
+            </Grid>
             </div>
         )
     }
