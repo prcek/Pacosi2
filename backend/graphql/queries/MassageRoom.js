96ef6c74eb0ccfdce32fbf38ac0215124c907f14
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-01-16 12:54:48 +0100

    massageroomday infos

--- backend/graphql/queries/MassageRoom.js
+++ backend/graphql/queries/MassageRoom.js
@@ -7,9 +7,45 @@ const {
     GraphQLNonNull,
 } = GraphQL;
 
+const GraphQLIsoDate = require('graphql-iso-date');
+const {
+    GraphQLDate,
+    GraphQLTime,
+    GraphQLDateTime
+} = GraphQLIsoDate;
+
 const MassageRoomType = require('../types/MassageRoom');
 const MassageRoomResolver = require('../resolvers/MassageRoom');
+const OpeningTimeType = require('../types/OpeningTime');
+const OpeningTimeResolver = require('../resolvers/OpeningTime');
+
 const BaseQuery = require('./BaseQuery');
+
+const DayStatusType =  new GraphQL.GraphQLEnumType({
+    name: 'DayStatus',
+    values: {
+        OFF:{value:0},
+        FREE:{value:1},
+        BUSY:{value:2}
+    }
+});
+
+
+const MassageRoomDayInfoType = new GraphQL.GraphQLObjectType({
+    name: 'MassageRoomDayInfo',
+   
+
+    fields: () => ({
+        date: {
+            type: GraphQLDate,
+        },
+        status: {
+            type: DayStatusType
+        }
+    })
+
+});
+
 
 class MassageRoomQuery extends BaseQuery {
 
@@ -17,6 +53,68 @@ class MassageRoomQuery extends BaseQuery {
         super(MassageRoomType,MassageRoomResolver);
     }
 
+    dayInfos() {
+        return {
+            type: new GraphQLList(MassageRoomDayInfoType),
+            description: 'This will return all the items present in the database',
+            args: {
+                massage_room_id: {
+                    type: new GraphQLNonNull(GraphQLID),
+                    description: 'Please enter massage room id',
+                },
+                begin_date: {
+                    type: new GraphQLNonNull(GraphQLDate),
+                    description: 'Please enter  begin date',
+                },
+                end_date: {
+                    type: new GraphQLNonNull(GraphQLDate),
+                    description: 'Please enter end date',
+                }
+            },
+            resolve(parent, args, context, info) {
+                return MassageRoomResolver.dayInfos(args);
+            }
+        }
+    }
+
+    openingTimes() {
+        return {
+            type: new GraphQLList(OpeningTimeType),
+            description: 'This will return all the items present in the database',
+            args: {
+                massage_room_id: {
+                    type: new GraphQLNonNull(GraphQLID),
+                    description: 'Please enter massage room id',
+                },
+                /*
+                surgery_room_id: {
+                    type: GraphQLID,
+                    description: 'Please enter surgery room id',
+                },
+                */
+                date: {
+                    type: new GraphQLNonNull(GraphQLDate),
+                    description: 'Please enter  date',
+                }
+            },
+            resolve(parent, args, context, info) {
+                let srch = {};
+                if (args.massage_room_id) {
+                    srch.massage_room_id=args.massage_room_id
+                }
+                /*
+                if (args.surgery_room_id) {
+                    srch.surgery_room_id=args.surgery_room_id
+                }
+                */
+                if (args.date) {
+                    srch.begin={"$gte":args.date,"$lt":new Date(args.date.getTime()+ (24 * 60 * 60 * 1000))}
+                }
+                return OpeningTimeResolver.index(srch);
+            }
+        }
+    }
+    
 }
 
 module.exports = new MassageRoomQuery();
--- backend/graphql/resolvers/MassageRoom.js
+++ backend/graphql/resolvers/MassageRoom.js
@@ -3,6 +3,13 @@
 const MassageRoom = require('../../services/models/MassageRoom');
 
 const BaseController = require('./BaseController');
+const OpeningTimeResolver = require('./OpeningTime');
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+
+const moment = MomentRange.extendMoment(Moment);
+
+
 
 class MassageRoomController extends BaseController {
 
@@ -23,6 +30,41 @@ class MassageRoomController extends BaseController {
     count(filter={}) { 
         const f = {...filter,...this.hiddenFilter}
         return super.count(f);
+    }
+
+    dayInfos(args) {
+
+        return new Promise((resolve, reject) => {
+
+            let srch = {};
+            if (args.massage_room_id) {
+                srch.massage_room_id=args.massage_room_id
+            }
+            if (args.begin_date && args.end_date) {
+                srch.begin={"$gte":args.begin_date,"$lt":args.end_date}
+            }
+            OpeningTimeResolver.index(srch).then(ots=>{
+
+                const range = moment.range(args.begin_date,args.end_date);
+                const days = Array.from(range.by('day'));
+                const infos = days.map(d=>{
+
+                    const has_ot = ots.find(ot=>{
+                        return d.isSame(moment(ot.begin),'day')
+                    });
+                    var status = 0;
+                    if (has_ot) {
+                        status = 1;
+                    } 
+
+                    return {date:d.toDate(),status:status};
+                })
+                resolve(infos);
+            }).catch(reject)
+    
+
+        });
+
     }
 
 };
