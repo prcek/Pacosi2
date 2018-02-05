ff70e1957dcbbfc97cac39b3d87d8f3b906a33c6
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-02-05 15:20:44 +0100

    login auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../services/models/User');
+
+
 /* login. */
 router.post('/login', function(req, res, next) {
   console.log("DO LOGIN");
-  console.log(req.body);
-  
-  res.json({
-  	id: 1,
-    login: req.body.login,
-    role: "xx",
-    auth: "xx",
-    auth_ok: req.body.password=="nimda",
-  });
+  //console.log(req.body);
+  const login = req.body.login;
+  const password = req.body.password;
+  console.log("DO LOGIN",login);
+  User.findOne({ login: login, hidden: {$ne: true}, status:1 }).then(r=>{
+    console.log(r);
+    const ok = r.comparePassword(password);
+    console.log("password is",ok);
+    res.json({
+      login:login,
+      auth_ok:ok,
+      role:r.role,
+    });
+  }).catch(err=>{
+    console.log("catch")
+    res.json({
+      login: login,
+      auth_ok: false,
+    });
+  })
+
+
 });
 
 module.exports = router;
