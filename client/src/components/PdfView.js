4e11ef1379898535ab60b7ab16ec8368df098087
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-02-13 17:04:08 +0100

    pdf gen

--- client/src/components/PdfView.js
+++ client/src/components/PdfView.js
@@ -2,58 +2,86 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import { compose } from 'react-apollo'
-import jsPDF from 'jspdf';
-require('jspdf-autotable');
+//import jsPDF from 'jspdf';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+//require('jspdf-autotable');
+
+//require('pdfmake/build/pdfmake.js'); 
+//require('pdfmake/build/vfs_fonts.js');
 
 const styles = theme => ({
     root: {
-      marginTop: theme.spacing.unit * 3,
-      width: '100%',
+     // marginTop: theme.spacing.unit * 3,
+      width: '800px',
       height: '500px',
     },
 });
   
 
 
+
 class PdfView extends React.Component {
+    state = {
+        datauristring: null
+    }
 
-
-    getPdf() {
-
-        var columns = ["ID", "Country", "Rank", "Capital"];
-        var data = [
-            [1, "Denmark", 7.526, "Copenhagen"],
-            [2, "Switzerland", 	7.509, "Bern"],
-            [3, "Iceland", 7.501, "Reykjavík"],
-            [4, "Norway", 7.498, "Oslo"],
-            [5, "Finland", 7.413, "Helsinki"],
-            [1, "Denmark", 7.526, "Copenhagen"],
-            [2, "Switzerland", 	7.509, "Bern"],
-            [3, "Iceland", 7.501, "Reykjavík"],
-            [4, "Norway", 7.498, "Oslo"],
-            [5, "Finland", 7.413, "Helsinki"]
-        ];
-
-        var doc = new jsPDF();
-        doc.autoTable(columns, data, {startY: 50, showHeader: 'firstPage'});
-        
-        
-        doc.setProperties({
-            title: 'Example: X' ,
-            subject: 'A jspdf-autotable example pdf'
-        });
-        //doc.autoPrint();
-        const pdfstring = doc.output('datauristring');
-        return pdfstring;
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.genPdf();
     }
 
 
+
+    genPdf() {
+        console.log("pdf render start")
+        var docDefinition = {
+            info: {
+                title: this.props.title,
+                //author: 'CLR Evidence',
+                //subject: 'subject of document',
+                //keywords: 'keywords for document',
+            },
+            content: [ 
+                {text: this.props.title, style: "header"},
+                this.props.description,
+
+            ],
+            styles: {
+                header: {
+                    fontSize: 18,
+                    bold: true,
+                    margin: [0, 0, 0, 10]
+                },
+                subheader: {
+                    fontSize: 16,
+                    bold: true,
+                    margin: [0, 10, 0, 5]
+                },
+                tableExample: {
+                    margin: [0, 5, 0, 15]
+                },
+                tableHeader: {
+                    bold: true,
+                    fontSize: 13,
+                    color: 'black'
+                }
+            },
+         };
+        const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+        pdfDocGenerator.getDataUrl((dataUrl) => {
+            this.setState({datauristring:dataUrl});
+        });
+    }
+
     render() {
         const { classes } = this.props;
-        const src = this.getPdf();
         return (
             <div className={classes.root}>
-                <embed width="100%" height="100%" name="plugin" id="plugin" src={src} type="application/pdf"/>
+                {this.state.datauristring && (
+                    <embed width="100%" height="100%" name="plugin" id="plugin" src={this.state.datauristring} type="application/pdf"/>
+                )}
             </div>
         )
     }
@@ -62,6 +90,10 @@ class PdfView extends React.Component {
 
 PdfView.propTypes = {
     classes: PropTypes.object.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    cols: PropTypes.arrayOf(PropTypes.string).isRequired,
+    rows: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string,PropTypes.number])).isRequired
 };
   
 
--- client/src/components/TestComponent.js
+++ client/src/components/TestComponent.js
@@ -8,6 +8,12 @@ import RoleField from './RoleField';
 import ClientView from './ClientView';
 import ClientField from './ClientField';
 import PdfView from './PdfView';
+
+//import  loremIpsum from 'lorem-ipsum';
+
+
+const czechText = "Daleko, za Slovnímy horami, vzdálené od země samohlásek a souhlásek žijí Smyšlené texty. Opuštěné žijí v Písmenkově na pobřeží Sémantiky, velkého Jazykooceánu. Místem protéká potůček zvaný Ottův slovník naučný a zásobuje ho potřebnými pravidly. Je to rajská země, ve které létají pečené části vět do úst. A ani od mocné Interpunkce nemohou být Smyšlené texty ovládány – takřka nepravopisný život. Jednoho dne se jeden malý řádek smyšleného textu rozhodl, jmenoval se Lorem Ipsum, vyjít si do rozlehlé gramatiky. Velký Oxmox mu to rozmlouval, jelikož se to tam hemží zlými Čárkami, divokými Otazníky a lstivými Středníky, ale Smyšlený textík se nenechal zmátnout. Zbalil si jeho Verzálky (Velká písmena), strčil si jeho Iniciály za opasek a dal se na cestu. Jakmile se vyšplhal na první pahorek Kurziva pohoří, podíval se naposledy na siluetu rodného města Písmenkova, nápisu Abecedyves a řádku jeho vlastní ulice, uličky Řádková. Lítostně mu tekla rétorická Otázka po tváři, pak pokračoval v cestě. Po cestě potkal Průklep. Průklep varoval Smyšlený textík že tam, odkud on přichází, by byl nekonečně opisován a vše co by z jeho původu zbylo, by bylo slovo „a“ a tak se má Smyšlený textík otočit a opět se vrátit do vlastní, bezpečné země. Ale všechny";
+
 //import Paper from 'material-ui/Paper';
 
 const styles = theme => ({
@@ -46,7 +52,7 @@ class TestComponent extends React.Component {
             <div>
             
 
-            <PdfView />
+            <PdfView title="Název dokumentu" description={czechText} cols={["id","dd"]} rows={[]}/>
 
             <ClientView client_id={this.state.client_id} />
             <ClientField id="cxx" name="cxxxx" value={this.state.client_id} onChange={this.handleClientChange}/>
