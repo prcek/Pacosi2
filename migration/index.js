5c6308c791b3065b2abaf5dc8b5d14fdbc525abe
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-02-14 23:10:54 +0100

    gql connect

--- migration/index.js
+++ migration/index.js
@@ -1,5 +1,50 @@
 
 require('dotenv').config()
+
+var ApolloClient = require('apollo-client').ApolloClient;
+var  HttpLink  = require('apollo-link-http').HttpLink;
+var  InMemoryCache = require('apollo-cache-inmemory').InMemoryCache;
+var fetch = require('node-fetch');
+var gql = require('graphql-tag');
+const baseUrl="http://localhost:4001";
+
+function doAuth() {
+
+    return new Promise(function(resolve, reject) {
+
+        fetch(baseUrl+"/auth/login",{
+            method:'POST',
+            body:JSON.stringify({login:process.env.GQL_USER,password:process.env.GQL_PASSWORD}), 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((resp) => resp.json()).then(data=>{
+            if (data.auth_ok) {
+                resolve(data.auth_token);
+            } else {
+                resolve(null);
+            }
+        })
+    
+    });
+}
+
+
+doAuth().then(auth=>{
+
+    const client = new ApolloClient({
+        link: new HttpLink({ uri: baseUrl+'/graphql',fetch:fetch, headers:{ authorization:"Bearer "+auth}}),
+        cache: new InMemoryCache()
+    });
+      
+    const q = gql`{ users{name}}`;
+      
+    client.query({ query: q }).then(console.log);
+      
+});
+
+/*
+require('dotenv').config()
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -11,9 +56,11 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+connection.query('SELECT surname FROM klienti', function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+  console.log('The solution is: ', results[0].surname);
 });
  
 connection.end();
+*/
+
